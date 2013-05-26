604bd8e65ff8e0bc4f1235d77b3b727ffd82e63a
Joseph Gibbs <manaorb@gmail.com>
2013-05-25 21:14:32 -0500

    FScene is now an FContainer and removed FrameCount variable

--- FLayer.cs
+++ FLayer.cs
@@ -1,11 +1,11 @@
 /*
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
+- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+- THE SOFTWARE.
 */
 
 using System;
@@ -15,40 +15,49 @@ using System.Collections.Generic;
 
 public class FLayer : FContainer, FMultiTouchableInterface
 {
-	protected int mFrameCount = 0;
-	
 	protected FScene mParent;
 	public FScene Parent
 	{
 		get { return mParent; }
 		set { mParent = value; }
 	}
-	
+
 	public FLayer (FScene _parent)
 	{
 		mParent = _parent;
 	}
-	
+
 	override public void HandleAddedToStage()
 	{
 		base.HandleAddedToStage();
 		Futile.touchManager.AddMultiTouchTarget(this);
-		Futile.instance.SignalUpdate += HandleUpdate;	
+		Futile.instance.SignalUpdate += Update;
+		
+		this.Start();
+		
+		Debug.Log("+++ Added To Stage");
 	}
-	
+
 	override public void HandleRemovedFromStage()
 	{
 		Futile.touchManager.RemoveMultiTouchTarget(this);
-		Futile.instance.SignalUpdate -= HandleUpdate;
-		base.HandleRemovedFromStage();	
+		Futile.instance.SignalUpdate -= Update;
+		base.HandleRemovedFromStage();
+		
+		Debug.Log("--- Removed From Stage");
 	}
-	
-	virtual protected void HandleUpdate ()
+
+	virtual public void Update ()
 	{
-		mFrameCount++;
+		
 	}
 	
-	public void HandleMultiTouch(FTouch[] touches)
+	virtual public void HandleMultiTouch(FTouch[] touches)
+	{
+
+	}
+	
+	virtual public void Start()
 	{
 		
 	}
--- FSceneManager.cs
+++ FSceneManager.cs
@@ -1,11 +1,11 @@
 /*
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
+- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+- THE SOFTWARE.
 */
 
 using System;
@@ -16,10 +16,10 @@ using System.Collections.Generic;
 public sealed class FSceneManager : FContainer
 {
 	private static readonly FSceneManager mInstance = new FSceneManager();
-	
+
 	private List<FScene> mScenes;
 	public static FStage mStage;
-	
+
 	public static FSceneManager Instance
 	{
 		get
@@ -27,37 +27,37 @@ public sealed class FSceneManager : FContainer
 			return mInstance;
 		}
 	}
-	
-	private FSceneManager()
+
+	private FSceneManager() : base()
 	{
 		mScenes = new List<FScene>();
 		mStage = Futile.stage;
-		
+
 		mStage.AddChild(this);
 	}
-	
+
 	public void PushScene(FScene _scene)
 	{
 		mScenes.Add(_scene);
 		this.AddChild(_scene);
 	}
-	
+
 	public void PopScene()
 	{
 		if(mScenes.Count > 0)
 		{
 			FScene scene = mScenes[mScenes.Count - 1];
 			scene.RemoveFromContainer();
-			
+
 			mScenes.Remove(scene);
 		}
 	}
-	
+
 	public void SetScene(FScene _scene)
 	{
 		while(mScenes.Count > 0)
 			PopScene();
-		
+
 		PushScene(_scene);
 	}
 }
